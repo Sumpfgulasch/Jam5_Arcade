ca8899f49f1534f6507124f33df4ebaaedd93970
Julian Hellmann <kontakt@julianhellmann.de>
2025-04-30 15:46:39 +0200

    dotween

--- Jam5_Arcade/Scripts/NewMonoBehaviourScript.cs
+++ Jam5_Arcade/Scripts/NewMonoBehaviourScript.cs
@@ -28,16 +28,17 @@ Scripts
     - onStart: fire a placeholder AudioEvent, using AudioSystem.Play2DAudio (will be AudioEvent.PlayerMovement, doesnt exist yet)
     - mouse input rotates the player to the mouse position; has max rotation speed (public); deals with delta time
     - movementSpeed tracks the speed of the rotation; is sent to FMOD using AudioSystem.SetLocalParameter
-    - if the "body" part collides with an enemy (probably use tags or layers), then placeholder AudioSystem.Play2DAudio call & game over screen
+    - if the "body" part collides with an enemy (probably use tags or layers), then placeholder AudioSystem.Play2DAudio (will be AudioEvent.DestroyEnemy) call & game over screen
     - if the "beam" part collides with an enemy (probalby use tags or layers), the enemy is destroyed
 
 - Enemy
     - init function: gets "speed" from outside
+    - OnStart: 
     - Update: moves the transform into the direction of the player with a fixed speed ("speed")
 - EnemyManager
     - public spawnCircleRadius field; please visualize it in the Unity Editor with a gizmo circle
     - spawns a small group of enemies (enemyCount) every few seconds (spawnInterval)
-        - assigns "enemySpeed" to enemy on instantiation
+        - assigns "enemySpeed" to enemy on instantiation; play placeholder AudioSystem.Play2DAudio call (will be AudioEvent.EnemySpawn)
         - the position of the first enemy of a group is always random on the spawn circle (defined by spawnCircleRadius, center is the screen center)
         - the spawn positions of the following enimies are slightly offset into one direction and slightly offset in time (spawnIntervalWithinGroup)
         - the enemies of a group are spawned as children of a newly created transform (enemyGroupTransform); keep track of that transform
